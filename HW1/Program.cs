d4d1803257b0562d8e9e0bdf027cf41ca129053a
Oliaoliaol <o_safina@mail.ru>
2022-10-04 19:25:45 +0300

    1 задача на макс дописать

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,1 +1,16 @@
 ﻿Console.WriteLine("Hello, World!");
+/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+Console.WriteLine(a = 5; b = 7);
+int a= Convert.ToInt32(Console.ReadLine());
+int b= Convert.ToInt32(Console.ReadLine());
+int max;
+if (a>b);
+{
+    max=a
+}
+else max=b;
+Console.WriteLine(max);
