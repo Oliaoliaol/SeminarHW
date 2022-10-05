ee5c7b44f864125e82d9be38f09e730d966e0ddd
Oliaoliaol <o_safina@mail.ru>
2022-10-05 20:42:29 +0300

    первая задача

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,16 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
-/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */
-Console.WriteLine(a = 5; b = 7);
-int a= Convert.ToInt32(Console.ReadLine());
-int b= Convert.ToInt32(Console.ReadLine());
+Console.Write("Запишите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Запишите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
 int max;
-if (a>b);
+if (a>b)
 {
-    max=a
+  max=b;
 }
-else max=b;
-Console.WriteLine(max);
+else max=a; 
+
+Console.WriteLine($"Наибольшее число {b}, Наименьшее число {a}");
