b64a86140d460963bf73a69b2c3f76fb244efc5d
Oliaoliaol <o_safina@mail.ru>
2022-10-05 21:08:08 +0300

    вторая задача

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -16,3 +16,29 @@ else max=a;
 
 Console.WriteLine($"Наибольшее число {b}, Наименьшее число {a}");
 */
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Введите первое число: ");
+int n1=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второ число: ");
+int n2=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int n3=Convert.ToInt32(Console.ReadLine());
+int max;
+if (n1>n2)
+{
+   max=n1;
+}
+else max=n2;
+if (n2>n3)
+{
+   max=n2;
+}
+else max=n3;
+if (n1>n3)
+{
+   max=n1;
+}
+else max=n3;
+Console.WriteLine("Наибольшее число " + max);
+
