dd374d0ea5453e81fda650317042a904aa0af581
Oliaoliaol <o_safina@mail.ru>
2022-10-05 21:06:28 +0300

    первая задача

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -3,7 +3,7 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */
-Console.Write("Запишите первое число: ");
+/*Console.Write("Запишите первое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Запишите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
@@ -15,3 +15,4 @@ if (a>b)
 else max=a; 
 
 Console.WriteLine($"Наибольшее число {b}, Наименьшее число {a}");
+*/
