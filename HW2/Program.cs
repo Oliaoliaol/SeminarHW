d2e5c0592f2b1205af968335065844fbe5d060b7
Oliaoliaol <o_safina@mail.ru>
2022-10-08 13:19:47 +0300

    Первая задача второго семинар

---
+++ HW2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+/*Console.Write("Ведите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >=100 && num < 1000)
+{
+    int LastDigit = num /10 %10;
+    Console.WriteLine($"Вторая цифра трехзначного числа это {num} is {LastDigit}");
+}
+else 
+    Console.WriteLine("Некорректное число");
+*/
+
