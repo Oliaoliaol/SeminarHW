d7acaeeaa3f72039bbcfa371a47c4883275867d3
Oliaoliaol <o_safina@mail.ru>
2022-10-10 21:24:38 +0300

    вторя задача третьего семинара

--- HW3/Program.cs
+++ HW3/Program.cs
@@ -16,4 +16,29 @@ if (num!.Length==5)
 else Console.Write($"Введи правильное число");
 */
 //Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+/*double Distance(double x1, double x2, double y1, double y2, double z1, double z2)
+{
+    double a = Math.Pow(x2-x1,2);
+    double b = Math.Pow(y2-y1,2);
+    double c = Math.Pow(z2-z1,2);
+    double d = a+b+c;
+    d =Math.Sqrt(d);
+    return d;
+}
+Console.Write("Введите x перdой точки: ");
+double number1=Convert.ToDouble(Console.ReadLine());
+Console.Write("Ввидете y первой точки: ");
+double number2=Convert.ToDouble(Console.ReadLine());
+Console.Write ("Введите z первой точки: ");
+double number3=Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите x второй точки: ");
+double number4=Convert.ToDouble(Console.ReadLine());
+Console.Write("Ввидете y второй точки: ");
+double number5=Convert.ToDouble(Console.ReadLine());
+Console.Write ("Введите z второй точки: ");
+double number6=Convert.ToDouble(Console.ReadLine());
+
+Console.Write(Distance(number1,number2,number3, number4, number5, number6));
+*/
+
 
