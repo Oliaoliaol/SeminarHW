096841a8f2bc3b6945c5336f7a3e63b32515ecda
Oliaoliaol <o_safina@mail.ru>
2022-10-13 21:50:33 +0300

    Первая задача

---
+++ HW4/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+/*double Exponentiation(double a, double b)
+{
+double c = Math.Pow(a,b);
+return c;
+}
+Console.Write("Введите число: ");
+int a1 =Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int b1 =Convert.ToInt32(Console.ReadLine());
+Console.Write($"Ответ: {Exponentiation(a1,b1)}");
+*/
