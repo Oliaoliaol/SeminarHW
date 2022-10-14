16c7675682f983a02045e1cbb354b2a9480c12f5
Oliaoliaol <o_safina@mail.ru>
2022-10-14 19:34:01 +0300

    Вторая задача

--- HW4/Program.cs
+++ HW4/Program.cs
@@ -10,3 +10,22 @@ Console.Write("Введите число: ");
 int b1 =Convert.ToInt32(Console.ReadLine());
 Console.Write($"Ответ: {Exponentiation(a1,b1)}");
 */
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+/*int SumNumber(int num)
+{
+int res = 0;
+int sum = 0;
+while(num > 0 )
+    {
+        sum=num % 10;
+        num= num/10;
+        res = sum + res;
+    }
+    return res;
+}
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = SumNumber(a) ;
+Console.Write($"Сумма цифр в числе {a}: {b}");
+*/
