d7391d3a64d893f09bbbd505c6275eaf45c4b382
Oliaoliaol <o_safina@mail.ru>
2022-11-12 21:32:03 +0300

    третья задача

--- HW9/Program.cs
+++ HW9/Program.cs
@@ -21,7 +21,7 @@ int InputInt(string output)
 }
 */
 //Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
-int m = InputInt("Введите M:");
+/*int m = InputInt("Введите M:");
 int n = InputInt("Введите N:");
 Console.WriteLine($"Сумма элементов от {m} до {n} = {CountNaturalSum(m, n)}");
 int InputInt(string output)
@@ -34,4 +34,24 @@ int CountNaturalSum(int m, int n)
     if (m == n)
         return n;
     return n + CountNaturalSum(m, n - 1);
+}
+*/
+//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+Console.WriteLine("Введите два положительных числа: M и N.");
+int m = InputInt("Введите M: ");
+int n = InputInt("Введите N: ");
+Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
 }
